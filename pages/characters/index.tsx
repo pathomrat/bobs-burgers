0a45979c24023bd5921ebe914613a7f9d4d59bc2
pathomrat <pathomrat97@gmail.com>
2022-06-02 15:26:46 +0700

    :construction: Setting api

---
+++ pages/characters/index.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+
+const Characters: NextPage = () => {
+  const [characters, setCharacters] = useState<any[]>([]);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      const res = await axios.get(`https://bobsburgers-api.herokuapp.com/characters/`);
+      console.log(res.data)
+      setCharacters(res.data);
+    };
+    getCharacters();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {characters?.map((c) => (
+          <li key={c.id}>{c.id}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Characters;
